125286d7c54839ac3d28b7ac5761900eb1bb5421
Erik Portin <erikportin@gmail.com>
2016-03-26 07:00:12 +0100

    getTrack response as dict

--- src/ios/iOSAudioInfo.m
+++ src/ios/iOSAudioInfo.m
@@ -7,62 +7,65 @@
 
 - (void) getTracks:(CDVInvokedUrlCommand *)command
 {
-    MPMediaQuery *everything = [[MPMediaQuery alloc] init];
-    NSArray *itemsFromGenericQuery = [everything items];
-    songsList = [[NSMutableArray alloc] init];
-    for(MPMediaItem *song in itemsFromGenericQuery)
-    {
-        [songsList addObject:[self buildSongInfo:song:NO]];
-    }
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:songsList];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    [self.commandDelegate runInBackground:^{
+        MPMediaQuery *everything = [[MPMediaQuery alloc] init];
+        NSArray *itemsFromGenericQuery = [everything items];
+        songsList = [[NSMutableArray alloc] init];
+        
+        for(MPMediaItem *song in itemsFromGenericQuery){
+            [songsList addObject:[self buildSongInfo:song:NO]];            
+        }
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:songsList];      
+        NSLog(@"The content of arry is%@",songsList); 
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];        
 }
 - (void) getTrack:(CDVInvokedUrlCommand *)command
 {
-    NSString *trackId = [command argumentAtIndex:0];
-    
-    if(trackId == nil){
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"No ID found"];
-    }
-    else
-    {
-        MPMediaItem *song;
-        MPMediaPropertyPredicate *predicate;
-        MPMediaQuery *songQuery;
+    [self.commandDelegate runInBackground:^{ 
+        NSString *persistentID = [command argumentAtIndex:0];
         
-        predicate = [MPMediaPropertyPredicate predicateWithValue: trackId forProperty:MPMediaItemPropertyPersistentID];
-        songQuery = [[MPMediaQuery alloc] init];
-        [songQuery addFilterPredicate: predicate];
-        
-        if (songQuery.items.count > 0)
-        {
-            songsList = [[NSMutableArray alloc] init];
-            song = [songQuery.items objectAtIndex:0];
-            [songsList addObject:[self buildSongInfo:song:YES]];
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:songsList];
+        if(persistentID == nil){
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"No ID found"];
+        } else {
+            MPMediaItem *song;
+            MPMediaPropertyPredicate *predicate;
+            MPMediaQuery *songQuery;
+            
+            predicate = [MPMediaPropertyPredicate predicateWithValue: persistentID forProperty:MPMediaItemPropertyPersistentID];
+            songQuery = [[MPMediaQuery alloc] init];
+            [songQuery addFilterPredicate: predicate];
+            
+            if (songQuery.items.count > 0){
+                song = [songQuery.items objectAtIndex:0];
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:[self buildSongInfo:song:YES]];
+            } else {
+                pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"track not found"];
+            }        
         }
-        else
-        {
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"track not found"];
-        }        
-    }
 
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
 }
 
-- (NSMutableDictionary *)buildSongInfo :(MPMediaItem*)song :(BOOL*)addImage
+- (NSMutableDictionary *)buildSongInfo :(MPMediaItem*)song :(BOOL)addImage
 {
     NSString *title = [song valueForProperty:MPMediaItemPropertyTitle];
     NSString *albumTitle = [song valueForProperty:MPMediaItemPropertyAlbumTitle];
     NSString *artist = [song valueForProperty:MPMediaItemPropertyArtist];
     NSString *genre = [song valueForProperty:MPMediaItemPropertyGenre];
-    NSString *trackId = [song valueForProperty:MPMediaItemPropertyPersistentID];
-    NSString *albumId = [song valueForProperty:MPMediaItemPropertyAlbumPersistentID];
+    NSString *persistentID = [song valueForProperty:MPMediaItemPropertyPersistentID];
+    NSString *albumPersistentID = [song valueForProperty:MPMediaItemPropertyAlbumPersistentID];
+    NSString *playCount = [song valueForProperty:MPMediaItemPropertyPlayCount];
+    NSString *rating = [song valueForProperty:MPMediaItemPropertyRating];
     
     NSLog(@"title = %@",title);
     NSLog(@"albumTitle = %@",albumTitle);
+    NSLog(@"albumPersistentID = %@",albumPersistentID);
     NSLog(@"artist = %@",artist);
-    
+    NSLog(@"rating = %@",rating);
+    NSLog(@"playCount = %@",playCount);
+
     NSMutableDictionary *songInfo = [[NSMutableDictionary alloc] init];
     if(title != nil) {
         [songInfo setObject:title forKey:@"title"];
@@ -84,17 +87,28 @@
     } else {
         [songInfo setObject:@"No genre" forKey:@"genre"];
     }
-    if(trackId !=nil) {
-        [songInfo setObject:trackId forKey:@"trackId"];
+    if(persistentID !=nil) {
+        NSLog(@"persistentID = %@",persistentID);
+        [songInfo setObject:[NSString stringWithFormat:@"%@", persistentID] forKey:@"persistentID"];
     } else {
-        [songInfo setObject:@"No trackId" forKey:@"trackId"];
+        [songInfo setObject:@"No persistentID" forKey:@"persistentID"];
     }
-    if(albumId !=nil) {
-        [songInfo setObject:albumId forKey:@"albumId"];
+    if(albumPersistentID !=nil) {
+        [songInfo setObject:[NSString stringWithFormat:@"%@", albumPersistentID] forKey:@"albumPersistentID"];
     } else {
-        [songInfo setObject:@"No albumId" forKey:@"albumId"];
+        [songInfo setObject:@"No albumPersistentID" forKey:@"albumPersistentID"];
     }
-    
+    if(playCount !=nil) {
+        [songInfo setObject:[NSString stringWithFormat:@"%@", playCount] forKey:@"playCount"];
+    } else {
+        [songInfo setObject:@"No playCount" forKey:@"playCount"];
+    }
+    if(rating !=nil) {
+        [songInfo setObject:[NSString stringWithFormat:@"%@", rating] forKey:@"rating"];
+    } else {
+        [songInfo setObject:[NSString stringWithFormat:@"%@", @"-1"] forKey:@"rating"];
+    }
+
     if(addImage){
         BOOL artImageFound = NO;
         NSData *imgData;
@@ -105,7 +119,7 @@
             artImageFound = YES;
         }
         if (artImageFound) {
-            [songInfo setObject:[imgData base64EncodedString] forKey:@"image"];
+            [songInfo setObject:[imgData base64EncodedStringWithOptions:0] forKey:@"image"];
         } else {
             [songInfo setObject:@"No Image" forKey:@"image"];
         }
--- www/iOSAudioInfo.js
+++ www/iOSAudioInfo.js
@@ -1,10 +1,11 @@
 var exec = require('cordova/exec');
 
 exports.getTracks = function(success, error) {
-    console.log("Plugin called");
+    console.log("getTracks called");
     exec(success, error, "iOSAudioInfo", "getTracks");
 };
 
 exports.getTrack = function(success, error, trackId) {
+    console.log("getTrack called", trackId);
     exec(success, error, "iOSAudioInfo", "getTrack", [trackId]);
 };
